fe9663dddf7863e9b2327a5a44c6bec7240fd1f7
Guilherme Magalhaes <silcol455@gmail.com>
2025-06-13 23:25:51 -0300

    Add dependency on master role in Step Functions module

--- infra/04_stepfunctions.tf
+++ infra/04_stepfunctions.tf
@@ -3,4 +3,6 @@ module "step_functions" {
 
   definitions_directory = "${path.module}/definitions/stepfunctions"
   role_arn              = module.master_role.role_arn
+
+  depends_on = [ module.master_role ]
 }
