5410fa6e7458a7dec487d4a76f6254ec3b1f88ca
Guilherme Magalhaes <silcol455@gmail.com>
2025-06-14 00:02:29 -0300

    Update .gitignore and refactor infrastructure configuration for region variable

--- infra/00_vars.tf
+++ infra/00_vars.tf
@@ -1,4 +1,11 @@
 
+variable "region" {
+    description = "AWS region to deploy the infrastructure"
+    type        = string
+    default     = "us-east-2"
+  
+}
+
 variable "template_values" {
     description = "Template values for the infrastructure modules"
     type        = map(string)
